8d97fafc0ffca0bd6330c9e91922e1203ff223d7
Maik Henrique <maik.hsp.contato@gmail.com>
2020-10-03 21:35:35 -0300

    first commit

---
+++ src/main/java/com/dhspringsecurity/dhspringsecurity/DhSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.dhspringsecurity.dhspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhSpringSecurityApplication.class, args);
+	}
+
+}
